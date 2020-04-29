8f3300728d54fcbfe563b12e24b81bde4f6388a3
kimtaehyeon <thxogusrla@gmail.com>
2020-04-29 23:05:14 +0900

    remark

--- 음악스트리밍사이트2/js/script.js
+++ 음악스트리밍사이트2/js/script.js
@@ -4,10 +4,13 @@ $('.to-top-btn').on('click', function(){
 });
 
 function scrollerHandler(){
-  var windowBottom = $(window).scrollTop() + $(window).height();
-  $('.playlist').each(function(){
+
+  var windowBottom = $(window).scrollTop() + $(window).height(); //스크롤 할때마다 $(window).scrollTop()의 값이 변함.
+  $('.playlist').each(function(){ //playlist 클래스인 것들을 모두 꺼냄.
     if(windowBottom >$(this).position().top + $(this).outerHeight() / 2){
-      $(this).animate({'opacity':'1'}, 1500);
+      //$(this).position().top은 해당 요소의 첫 window 줄부터 요소의 가장 윗 부분까지.
+      //$(this).outerHeight()는 해당 요소의 세로줄
+      $(this).animate({'opacity':'1'}, 1500); //1.5초가 거림.
     }
   });
   if(windowBottom == $(document).height()) { $('.to-top-btn').fadeIn(); }
--- 포트폴리오만들기/js/script.js
+++ 포트폴리오만들기/js/script.js
@@ -1,23 +1,28 @@
 $(window).on('scroll', scrollHandler);
 
-function scrollHandler(){
-  if($(window).scrollTop() >= $('.about').position().top){
+function scrollHandler(){ //화면에서 스크롤을 사용할 때 사용되는 함수.
+  if($(window).scrollTop() >= $('.about').position().top){ //scrollTop이 position().top보다 크면 지나친거니까 .about이 보이게 설정함.
+    // $('.skill').each(function(){
+    //   var skill = $(this); //자신의 skill들을 저장.
+    //   var percentages = skill.find('.percentage').text();
+    //   skill.find('.inner-bar').animate({width:percentages}, 2200);
+    // });
     $('.skill').each(function(){
-      var skill = $(this); //자신의 skill들을 저장.
-      var percentages = skill.find('.percentage').text();
-      skill.find('.inner-bar').animate({width:percentages}, 2200);
+      var skill = $(this);
+      var percentage = skill.find('.percentage').text();
+      skill.find('.inner-bar').animate({width:percentage}, 1500);
     });
-
     $('.menu-right button').css('color', '#4a4a4a');
   }else $('.menu-right button').css('color', 'white');
 
   $('section').each(function(){
     if($(window).scrollTop() >= $(this).position().top){
-      $(this).find('.vertical-center').animate({top:'0', opacity:'1'});
+      $(this).find('.vertical-center').animate({top:'0', opacity:'1'}); //section 안에 있는 vertical-center
     }
   });
-  
 }
+
+
 //$('html, body')로 설정은 모든 브라우저에서 사용할 수 있도록 설정함.
 $('.menu-right button').on('click', function(){
   var id = $(this).attr('id');
